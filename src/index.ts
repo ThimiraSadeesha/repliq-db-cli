e43c6d6a9d34687d0867f50336a2648ca43f2e4d
Thimira Sadeesha <thimira@maximimpressions.com>
2025-12-13 09:06:40 +0530

    added `backupDatabase` method for full backup and work as a library

--- src/index.ts
+++ src/index.ts
@@ -3,4 +3,5 @@ export * from './utils/connection';
 export * from './utils/prompts';
 export * from './commands/test';
 export * from './commands/backup';
+export * from './core/backup';
 export * from './commands/copy';
